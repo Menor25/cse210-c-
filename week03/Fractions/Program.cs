10a4163a70ca684f37f35f390e8dc8cdc1479b43
Theophilus Ajiri Menor <ajirimenor@gmail.com>
2026-01-24 13:13:15 +0100

    Week 3 fraction task done

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction1.GetFraction());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFraction());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFraction());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFraction());
     }
 }
