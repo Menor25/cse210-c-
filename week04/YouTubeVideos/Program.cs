bd529fe35598fd220339c5447579815883988a8a
Theophilus Ajiri Menor <ajirimenor@gmail.com>
2026-02-07 11:54:52 +0100

    Youtube video assignment done

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("How to Code in C#", "John Doe", 600);
+        video1.AddComment(new Comment("Alice", "This was so clear!"));
+        video1.AddComment(new Comment("Bob", "I finally understand classes."));
+        video1.AddComment(new Comment("Charlie", "Can you do one on Python?"));
+        videos.Add(video1);
+
+        Video video2 = new Video("Top 10 Programming Languages", "Jane Smith", 900);
+        video2.AddComment(new Comment("Dave", "Great list."));
+        video2.AddComment(new Comment("Eve", "I can't stop laughing."));
+        video2.AddComment(new Comment("Frank", "Is it difficult to learn?"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Minecraft Survival Ep. 1", "Gamer123", 1200);
+        video3.AddComment(new Comment("Grace", "Don't dig straight down!"));
+        video3.AddComment(new Comment("Heidi", "Nice house build."));
+        video3.AddComment(new Comment("Ivan", "When is the next episode?"));
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._lengthInSeconds} seconds");
+            
+            // Using the Abstraction method requested
+            Console.WriteLine($"Comment Count: {video.GetCommentCount()}"); 
+            Console.WriteLine("Comments:");
+
+            foreach (Comment c in video._comments)
+            {
+                Console.WriteLine($"- {c._name}: {c._text}");
+            }
+        }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+public class Video
+{
+    public string _title;
+    public string _author;
+    public int _lengthInSeconds;
+
+    public List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        _title = title;
+        _author = author;
+        _lengthInSeconds = lengthInSeconds;
+    }
+
+    public void AddComment(Comment newComment)
+    {
+        _comments.Add(newComment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+}
