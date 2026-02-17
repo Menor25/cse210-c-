a7614e88016898aa7f48c472cc394b17e1a43c31
Theophilus Ajiri Menor <ajirimenor@gmail.com>
2026-02-17 13:30:51 +0100

    Weej 6 assignment on polymorphism done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square color: {square.GetColor()}, area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle color: {circle.GetColor()}, area: {circle.GetArea()}");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}, area: {shape.GetArea()}");
+        }
     }
 }
